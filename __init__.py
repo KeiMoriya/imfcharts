18d7a6e203bd59ef7ae5f5de8a409a16cc6ca8e9
Moriya <KMoriya@imf.org>
2025-10-31 16:53:27 -0400

    Add imports from mpl, set style to imf-articleiv by default when importing imfcharts

--- __init__.py
+++ __init__.py
@@ -1,3 +1,11 @@
 
+import matplotlib.pyplot as plt
+
 from .charts import Chart
 from .charts import read
+
+from .mpl import *
+
+# Set default style
+# plt.style.use('imf-articleiv')
+set_style('imf-articleiv')
